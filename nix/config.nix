2dbd6bd66fee4f411b3d1ac7201d12f07d73216d
Nicole Venner <nvenner@protonmail.ch>
2025-01-02 09:06:44 -0700

    add a recursive flag for the copy

--- nix/config.nix
+++ nix/config.nix
@@ -10,7 +10,8 @@
   system.activationScripts.copyInitScript = lib.stringAfter [ "users" ] ''
     mkdir -p /mycorrhiza/
     rm -rf /mycorrhiza/infra
-    cp ${../.} /mycorrhiza/
+    cp -r ${../.} /mycorrhiza/
+    mkdir -p /mycorrhiza/infra/
     chmod +x /mycorrhiza/misc-setup.sh
     chown root:mycorrhiza /mycorrhiza -R
     chmod 775 /mycorrhiza -R
