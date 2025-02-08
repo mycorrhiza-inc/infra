47aaea0ad718e19e101174cda72cb848700ce716
Nicole Venner <nvenner@protonmail.ch>
2025-02-07 21:15:52 -0700

    podman compose not substitute for docker compose

--- nix/docker.nix
+++ nix/docker.nix
@@ -13,7 +13,7 @@
   environment.systemPackages = with pkgs; [
     dive # look into docker image layers
     podman-tui # status of containers in the terminal
-    # docker-compose # start group of containers for dev
+    docker-compose # start group of containers for dev
     podman-compose # start group of containers for dev
   ];
 }
