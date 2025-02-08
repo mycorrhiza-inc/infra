6be7b78f10a440dc500bee2e0a1524ba30def0dd
Nicole Venner <nvenner@protonmail.ch>
2025-02-07 20:25:39 -0700

    switch over to podman with fixed typos

--- nix/docker.nix
+++ nix/docker.nix
@@ -1,16 +1,13 @@
 { config, pkgs, lib, ... }: {
-  virtualisation.docker.enable = true;
-  virtualisation = {
-      podman = {
-        enable = true;
+  virtualisation.containers.enable = true;
+  virtualisation.podman = {
+    enable = true;
 
-        # Create a `docker` alias for podman, to use it as a drop-in replacement
-        dockerCompat = true;
-
-        # Required for containers under podman-compose to be able to talk to each other.
-        defaultNetwork.settings.dns_enabled = true;
-      };
-    };
+    # Create a `docker` alias for podman, to use it as a drop-in replacement
+    dockerCompat = true;
+    # Required for containers under podman-compose to be able to talk to each other.
+    defaultNetwork.settings.dns_enabled = true;
+  };
 
   # Useful other development tools
   environment.systemPackages = with pkgs; [
