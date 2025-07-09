70e98059ca5ba246f7bdb5f44c47957e2f401e1b
Nicole Venner <nvenner@protonmail.ch>
2025-07-09 17:20:23 -0600

    modify root checks for server

--- webhook-deploys/start-root-server.sh
+++ webhook-deploys/start-root-server.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
 # Install dependencies
 uv sync
 
 # Run the server as root
-sudo uv run --host 0.0.0.0 --port 8000
+uv run --host 0.0.0.0 --port 8000
